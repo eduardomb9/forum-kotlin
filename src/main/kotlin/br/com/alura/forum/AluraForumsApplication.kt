d62bc8a5b3c03b2516514ebe02dd7a2a3d03afe9
Eduardo Maranhão Braga <maranhaoeduardo9@gmail.com>
2021-09-27 12:56:59 -0300

    initial commit

---
+++ src/main/kotlin/br/com/alura/forum/AluraForumsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.alura.forum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraForumsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraForumsApplication>(*args)
+}
